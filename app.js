95dda683aefc9450c0dc6116b979483c8e1f39cc
Mariam-Davitadze-TGA <mariam.davitadze@tgadigital.com>
2021-06-16 17:29:57 +0400

    feature - added delete/edit functionality

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const submitText = (e) => {
 		newItem.innerHTML = `
                 <span> ${inputVal} </span>
                 <input type="checkbox" class="checkItem" onclick="markDone(event)">
+                <button class="removeItem" onclick="removeItem(event)">X</button>
+                <button class="editItem" onclick="editItem(event)">EDIT</button>
                 `
 		document.getElementById("myList").append(newItem)
 		document.getElementById("txt").value = ''
@@ -15,10 +17,46 @@ const submitText = (e) => {
 	}
 }
 
-const markDone = (event) => {
-	let item = event.target.parentElement;
-	let checkBox = event.target
+let beingEdited = false;
 
-	if(checkBox.checked) item.classList.add('active-item')
-	else item.classList.remove('active-item')
+const markDone = (event) => {
+	if (!beingEdited) {
+		let item = event.target.parentElement;
+		let checkBox = event.target
+
+		if (checkBox.checked) item.classList.add('active-item')
+		else item.classList.remove('active-item')
+	}
+}
+
+const removeItem = (event) => {
+	if (!beingEdited) {
+		let item = event.target.parentElement;
+		item.remove()
+	}
+}
+
+const editItem = (event) => {
+	beingEdited = !beingEdited
+	let item = event.target.parentElement
+	let child1 = item.firstElementChild
+	let checkBox = item.getElementsByTagName('input')[0]
+	let button = item.getElementsByTagName('button')[0]
+	let newElem
+	if (beingEdited) {
+		checkBox.disabled = true
+		button.disabled = true
+		newElem = document.createElement('input')
+		newElem.classList.add('editText')
+		newElem.type = 'text'
+		newElem.value = child1.innerHTML
+		item.replaceChild(newElem, child1)
+		newElem.focus()
+	} else {
+		checkBox.disabled = false
+		button.disabled = false
+		newElem = document.createElement('span')
+		newElem.innerHTML = child1.value
+		item.replaceChild(newElem, child1)
+	}
 }
