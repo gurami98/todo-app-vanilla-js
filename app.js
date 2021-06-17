16b99a28cc3dae038d2e9543076afc15b3209743
Mariam-Davitadze-TGA <mariam.davitadze@tgadigital.com>
2021-06-17 19:52:59 +0400

    Work in Progress

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
+let count = 0;
 const submitText = (e) => {
 	e.preventDefault();
 	let inputVal = document.getElementById("txt").value
 	if (inputVal.trim() !== '') {
 		let newItem = document.createElement('li')
-
 		newItem.innerHTML = `
-                <span ondblclick="editItem(event)"> ${inputVal} </span>
+                <span onclick="displayContent(event)" ondblclick="editItem(event)"> ${inputVal} </span>
                 <input type="checkbox" class="checkItem" onclick="markDone(event)">
                 <button class="removeItem" onclick="removeItem(event)">DELETE</button>
                 <button class="editItem" onclick="editItem(event)">EDIT</button>
                 `
+		newItem.children[0].setAttribute('id', count.toString())
+		count++;
 		document.getElementById("myList").append(newItem)
 		document.getElementById("txt").value = ''
 	} else {
@@ -19,19 +21,58 @@ const submitText = (e) => {
 
 let beingEdited = false;
 
+let contentContainer = document.getElementById('content-div')
+
+let id;
+const displayContent = (event) => {
+	let text = document.getElementById('window-text')
+	contentContainer.style.display = 'flex'
+	text.innerHTML = event.target.innerHTML
+	event.target.innerHTML = "shevcvale"
+	id = event.target.id
+	console.log(event.target.id)
+	console.log(event.target)
+}
+
+const closeWindow = (event) => {
+	contentContainer.style.display = 'none'
+	const contentDiv = document.getElementById('content-div')
+	const child2 = contentDiv.children[1]
+	console.log(contentDiv)
+	console.log(child2)
+	let targetEl = document.getElementById(id.toString())
+	if(child2.innerHTML === ''){
+		targetEl.parentElement.remove()
+	}else{
+		targetEl.innerHTML = child2.innerHTML
+	}
+}
+
 const markDone = (event) => {
 	if (!beingEdited) {
-		let line = event.target.parentElement;
 		let item = event.target.parentElement.firstElementChild;
 		let checkBox = event.target
 
 		if (checkBox.checked) {
 			item.classList.add('active-item')
-			line.classList.add('grayed-out')
 		}
 		else{
 			item.classList.remove('active-item')
-			line.classList.remove('grayed-out')
+		}
+	}
+}
+
+const markContainerDone = (event) => {
+	if (!beingEdited) {
+		const contentDiv = document.getElementById('content-div')
+		const child2 = contentDiv.children[1]
+		let checkBox = event.target
+
+		if (checkBox.checked) {
+			child2.classList.add('active-item')
+		}
+		else{
+			child2.classList.remove('active-item')
 		}
 	}
 }
@@ -45,8 +86,21 @@ const removeItem = (event) => {
 	}
 }
 
-const editItem = (event) => {
+const removeContainerItem = (event) => {
+	if (!beingEdited) {
+		if (confirm('Are you sure you want to delete this item')) {
+			const contentDiv = document.getElementById('content-div')
+			const child2 = contentDiv.children[1]
+			const child3 = contentDiv.children[2]
+			child2.innerHTML = ''
+			//child3.remove()
 
+		}
+	}
+}
+
+const editItem = (event) => {
+	console.log(event.target)
 	const item = event.target.parentElement
 	const child1 = item.firstElementChild
 	beingEdited = child1.nodeName === 'SPAN'
@@ -77,7 +131,44 @@ const editItem = (event) => {
 			button.disabled = false
 			newElem = document.createElement('span')
 			newElem.innerHTML = child1.value
+			newElem.addEventListener('dblclick', (e)=>{
+				editItem(e)
+			})
 			item.replaceChild(newElem, child1)
+		}else{
+			alert('Enter Text')
+		}
+	}
+}
+
+const editContainerItem = (event) => {
+	const contentDiv = document.getElementById('content-div')
+	const child2 = contentDiv.children[1]
+	console.log(child2)
+	beingEdited = child2.nodeName === 'SPAN'
+	let newElem
+	if (beingEdited) {
+		newElem = document.createElement('input')
+		newElem.classList.add('editText')
+		newElem.type = 'text'
+		newElem.value = child2.innerHTML
+		newElem.addEventListener("keyup", (event) => {
+			if (event.code === 'Enter') {
+				event.preventDefault();
+				editContainerItem(event)
+			}
+		});
+		contentDiv.replaceChild(newElem, child2)
+		newElem.focus()
+	} else {
+		let inputText = contentDiv.getElementsByTagName('input')[0]
+		if (inputText.value.trim() !== '') {
+			newElem = document.createElement('span')
+			newElem.innerHTML = child2.value
+			newElem.addEventListener('dblclick', (e)=>{
+				editContainerItem(e)
+			})
+			contentDiv.replaceChild(newElem, child2)
 		}else{
 			alert('Enter Text')
 		}
